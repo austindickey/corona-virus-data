cb520ce89a7b514bd07d95c0d99d880c0d3c91b7
austindickey <austindickey@me.com>
2020-03-18 17:21:57 -0500

    adding output data for world and some of the countries to choose from and beginning stages of country api call code

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -9,7 +9,37 @@ function callAPIWorld () {
 
         console.log(data)
 
+        $("#totalWorldCases").text("Total Cases: " + data.total_cases)
+        $("#totalWorldActiveCases").text("Active Cases: " + data.total_active_cases)
+        $("#totalWorldRecoverd").text("Recovered: " + data.total_recovered)
+        $("#totalWorldDeaths").text("Deaths: " + data.total_deaths)
+        $("#totalWorldUnresolved").text("Unresolved: " + data.total_unresolved)
+        $("#totalWorldNewToday").text("New Cases Today: " + data.total_new_cases_today)
+        $("#totalWorldDeathsToday").text("New Deaths Today: " + data.total_new_deaths_today)
+
     })
 }
 
 callAPIWorld()
+
+function callAPICountry() {
+    var country = $("#countrySelector option:selected").val()
+    var countryQuery = "https://thevirustracker.com/free-api?countryTotal=" + country
+
+    $.ajax({
+        url: countryQuery,
+        method: "GET"
+    }).then(function(response) {
+
+        console.log(response)
+                
+        var data = response.results[0]
+
+        // console.log(data)
+
+    })
+}
+
+$("#submit").on("click", function() {
+    callAPICountry()
+})
