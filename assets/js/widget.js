d19d476766bd5b8f943b04e55fc89153f1737db9
austindickey <austindickey@me.com>
2020-03-24 02:05:06 -0500

    changing news to display on page load

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -1,3 +1,16 @@
+var newsArray = []
+
+function countProperties(obj) {
+    var count = 0;
+
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            ++count;
+    }
+
+    newsArray[0] = count - 1
+}
+
 function callAPIWorld () {
     var worldQuery = "https://thevirustracker.com/free-api?global=stats"
 
@@ -15,23 +28,37 @@ function callAPIWorld () {
         $("#totalWorldNewToday").text("New Cases Today: " + data.total_new_cases_today)
         $("#totalWorldDeathsToday").text("New Deaths Today: " + data.total_new_deaths_today)
 
+        countProperties(news)
+
+        var artId = newsArray[0]
+
+        for (let i = 0; i < 30; i++) {
+            var current = news[artId]
+            // console.log(current)
+
+            var containerDiv = $("<div>")
+            containerDiv.addClass("newsArticle")
+            var newImg = $("<img>")
+            newImg.attr("src", current.image)
+            newImg.addClass("newsPics")
+            var newLink = $("<a>")
+            newLink.attr("href", current.url)
+            newLink.html("<h3>" + current.title + "</h3>")
+            var time = $("<p>")
+            time.html("<span>" + current.time + "</span>")
+
+            containerDiv.append(newImg)
+            containerDiv.append(title)
+            containerDiv.append(time)
+            $("#newsStories").append(containerDiv)
+
+            artId--
+        }
+
     })
 }
 
 callAPIWorld()
-
-var newsArray = []
-
-function countProperties(obj) {
-    var count = 0;
-
-    for(var prop in obj) {
-        if(obj.hasOwnProperty(prop))
-            ++count;
-    }
-
-    newsArray[0] = count - 1
-}
 
 function callAPICountry() {
     var country = $("#countrySelector option:selected").val()
@@ -52,34 +79,6 @@ function callAPICountry() {
         $("#totalCountryUnresolved").text("Unresolved: " + data.total_unresolved)
         $("#totalCountryNewToday").text("New Cases Today: " + data.total_new_cases_today)
         $("#totalCountryDeathsToday").text("New Deaths Today: " + data.total_new_deaths_today)
-
-        countProperties(news)
-
-        var artId = newsArray[0]
-
-        for (let i = 0; i < 30; i++) {
-            var current = news[artId]
-            console.log(current) // push news articles to the index page
-
-            console.log(current.image)
-
-            var containerDiv = $("<div>")
-            containerDiv.addClass("newsArticle")
-            var newImg = $("<img>")
-            newImg.attr("src", current.image)
-            newImg.addClass("newsPics")
-            var title = $("<h3>")
-            title.text("<a href='" + current.url + "' target='_blank'>" + current.title + "</a>")
-            var time = $("<p>")
-            time.text("<span>" + current.time + "</span>")
-
-            containerDiv.append(newImg)
-            containerDiv.append(title)
-            containerDiv.append(time)
-            $("#newsStories").append(containerDiv)
-
-            artId--
-        }
 
     })
 }
