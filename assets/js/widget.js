665dfb64ee62e66ddcc90684d0399139bdbd962f
austindickey <austindickey@me.com>
2020-03-24 02:44:05 -0500

    adding more countries, moving news back to inside submit button click

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -1,3 +1,27 @@
+function callAPIWorld () {
+    var worldQuery = "https://thevirustracker.com/free-api?global=stats"
+
+    $.ajax({
+        url: worldQuery,
+        method: "GET"
+    }).then(function(response) {
+        var data = response.results[0]
+         console.log(response);
+         
+
+        $("#totalWorldCases").text("Total Cases: " + data.total_cases)
+        $("#totalWorldActiveCases").text("Active Cases: " + data.total_active_cases)
+        $("#totalWorldRecoverd").text("Recovered: " + data.total_recovered)
+        $("#totalWorldDeaths").text("Deaths: " + data.total_deaths)
+        $("#totalWorldUnresolved").text("Unresolved: " + data.total_unresolved)
+        $("#totalWorldNewToday").text("New Cases Today: " + data.total_new_cases_today)
+        $("#totalWorldDeathsToday").text("New Deaths Today: " + data.total_new_deaths_today)
+
+    })
+}
+
+callAPIWorld()
+
 var newsArray = []
 
 function countProperties(obj) {
@@ -11,22 +35,25 @@ function countProperties(obj) {
     newsArray[0] = count - 1
 }
 
-function callAPIWorld () {
-    var worldQuery = "https://thevirustracker.com/free-api?global=stats"
+function callAPICountry() {
+    var country = $("#countrySelector option:selected").val()
+    var countryQuery = "https://thevirustracker.com/free-api?countryTotal=" + country
 
     $.ajax({
-        url: worldQuery,
+        url: countryQuery,
         method: "GET"
     }).then(function(response) {
-        var data = response.results[0]
+                
+        var data = response.countrydata[0]
+        var news = response.countrynewsitems[0]
 
-        $("#totalWorldCases").text("Total Cases: " + data.total_cases)
-        $("#totalWorldActiveCases").text("Active Cases: " + data.total_active_cases)
-        $("#totalWorldRecoverd").text("Recovered: " + data.total_recovered)
-        $("#totalWorldDeaths").text("Deaths: " + data.total_deaths)
-        $("#totalWorldUnresolved").text("Unresolved: " + data.total_unresolved)
-        $("#totalWorldNewToday").text("New Cases Today: " + data.total_new_cases_today)
-        $("#totalWorldDeathsToday").text("New Deaths Today: " + data.total_new_deaths_today)
+        $("#totalCountryCases").text("Total Cases: " + data.total_cases)
+        $("#totalCountryActiveCases").text("Active Cases: " + data.total_active_cases)
+        $("#totalCountryRecoverd").text("Recovered: " + data.total_recovered)
+        $("#totalCountryDeaths").text("Deaths: " + data.total_deaths)
+        $("#totalCountryUnresolved").text("Unresolved: " + data.total_unresolved)
+        $("#totalCountryNewToday").text("New Cases Today: " + data.total_new_cases_today)
+        $("#totalCountryDeathsToday").text("New Deaths Today: " + data.total_new_deaths_today)
 
         countProperties(news)
 
@@ -54,31 +81,6 @@ function callAPIWorld () {
 
             artId--
         }
-
-    })
-}
-
-callAPIWorld()
-
-function callAPICountry() {
-    var country = $("#countrySelector option:selected").val()
-    var countryQuery = "https://thevirustracker.com/free-api?countryTotal=" + country
-
-    $.ajax({
-        url: countryQuery,
-        method: "GET"
-    }).then(function(response) {
-                
-        var data = response.countrydata[0]
-        var news = response.countrynewsitems[0]
-
-        $("#totalCountryCases").text("Total Cases: " + data.total_cases)
-        $("#totalCountryActiveCases").text("Active Cases: " + data.total_active_cases)
-        $("#totalCountryRecoverd").text("Recovered: " + data.total_recovered)
-        $("#totalCountryDeaths").text("Deaths: " + data.total_deaths)
-        $("#totalCountryUnresolved").text("Unresolved: " + data.total_unresolved)
-        $("#totalCountryNewToday").text("New Cases Today: " + data.total_new_cases_today)
-        $("#totalCountryDeathsToday").text("New Deaths Today: " + data.total_new_deaths_today)
 
     })
 }
